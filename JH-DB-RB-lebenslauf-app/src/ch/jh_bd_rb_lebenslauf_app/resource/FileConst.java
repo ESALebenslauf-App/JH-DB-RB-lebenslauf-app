a8711a8a452ff86e3e41d4c4c9f8e6ea82c3ff35
jherzig <info@jherzig.ch>
2014-06-12 08:26:10 +0200

    Klassen im Packet Resource bereinigt keine Logick verändert

--- JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/resource/FileConst.java
+++ JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/resource/FileConst.java
@@ -1,5 +1,8 @@
 package ch.jh_bd_rb_lebenslauf_app.resource;
 
+import android.annotation.SuppressLint;
+
+@SuppressLint("SdCardPath")
 public class FileConst {
 	
 	private static final String  PDF_PATH = "/sdcard/android/obb";
