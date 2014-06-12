a18c8acb4b6862c7bbf1592dab772bdb5f70c4c1
jherzig <info@jherzig.ch>
2014-06-12 11:19:51 +0200

    if Statement hinzugefügt und angepasst neue Methode getBildungRows erstellt dieser wird ein Bildungs Objekt und eine Spalte übergeben es werden alle Datenbank einträge geladen bei denen der Spalten inhalt mit dem Objekt inhalt der angegebenen Spallte identisch ist geladen.

--- JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/daten/BildungDB.java
+++ JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/daten/BildungDB.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 /**
  * @author j.herzig
- *
+ * 
  */
 public class BildungDB implements LebenslaufDB {
 
@@ -37,8 +37,8 @@ public class BildungDB implements LebenslaufDB {
 
 		Cursor result = db.query(false, TABLE_BILDUNG, PROJECTION_BILDUNG,
 				BILDUNG_ID + "=?", dbID, null, null, null, null);
-		boolean found = result.moveToFirst();
-		if (found) {
+
+		if (result.moveToFirst()) {
 			return setBildung(bildung, result);
 		} else {
 			result.close();
@@ -46,27 +46,82 @@ public class BildungDB implements LebenslaufDB {
 		}
 	}
 
+	// Bildung ausgeben
+	public ArrayList<Bildung> getBildungRows(Bildung bildung, String columWhere) {
+		String[] dbWhere = new String[1];
+		String dbColWhere = BILDUNG_ID;
 
+		switch (columWhere) {
+		case BILDUNG_ID:
+			dbWhere[0] = bildung.getId().toString();
+			dbColWhere = BILDUNG_ID;
+			break;
+		case BILDUNG_BILDUNGSART:
+			dbWhere[0] = bildung.getAusbildungsart().toString();
+			dbColWhere = BILDUNG_BILDUNGSART;
+			break;
+		case BILDUNG_SCHULNAME:
+			dbWhere[0] = bildung.getNameschule().toString();
+			dbColWhere = BILDUNG_SCHULNAME;
+			break;
+		case BILDUNG_PLZ:
+			dbWhere[0] = bildung.getPlz().toString();
+			dbColWhere = BILDUNG_PLZ;
+			break;
+		case BILDUNG_ORT:
+			dbWhere[0] = bildung.getAdresseSchule().toString();
+			dbColWhere = BILDUNG_ORT;
+			break;
+		case BILDUNG_VON:
+			dbWhere[0] = bildung.getDatumVon().toString();
+			dbColWhere = BILDUNG_VON;
+			break;
+		case BILDUNG_BIS:
+			dbWhere[0] = bildung.getDatumBis().toString();
+			dbColWhere = BILDUNG_BIS;
+			break;
+
+		default:
+			break;
+		}
+
+		Cursor result = db.query(false, TABLE_BILDUNG, PROJECTION_BILDUNG,
+				dbColWhere + "=?", dbWhere, null, null, null, null);
+		
+		if (result.moveToFirst()) {
+			ArrayList<Bildung> bildungen = new ArrayList<Bildung>();
+			result.moveToFirst();
+			while (!result.isAfterLast()) {
+				Bildung resultbildung = new Bildung(result.getLong(0));
+				bildungen.add(setBildung(resultbildung, result));
+				result.moveToNext();
+			}
+			return bildungen;
+		} else {
+			result.close();
+			return null;
+		}
+	}
 
 	/**
 	 * @return ArrayList<Bildung>
 	 */
 	public ArrayList<Bildung> getAllBildungen() {
 		ArrayList<Bildung> bildungen = new ArrayList<Bildung>();
-		
+
 		Cursor result = db.query(false, TABLE_BILDUNG, PROJECTION_BILDUNG,
 				null, null, null, null, null, null);
 
-		result.moveToFirst();
-		while (! result.isAfterLast()) {
-			Bildung bildung = new Bildung(result.getLong(0));
-			bildungen.add(setBildung(bildung, result));
-			result.moveToNext();
+		if (result.moveToFirst()) {
+			while (!result.isAfterLast()) {
+				Bildung bildung = new Bildung(result.getLong(0));
+				bildungen.add(setBildung(bildung, result));
+				result.moveToNext();
+			}
 		}
-		
+
 		return bildungen;
 	}
-
 
 	/**
 	 * loescht einen Eintrag in der Tabelle Bildung
@@ -79,7 +134,6 @@ public class BildungDB implements LebenslaufDB {
 		dbID[0] = bildung.getId().toString();
 		return db.delete(TABLE_BILDUNG, BILDUNG_ID + "=?", dbID) > 0;
 	}
-
 
 	/**
 	 * erzeugt einen Eintrag in der Tabelle Bildung
@@ -101,13 +155,12 @@ public class BildungDB implements LebenslaufDB {
 		return bildung;
 	}
 
-	//TODO Testen und überarbeiten
+	// TODO Testen und überarbeiten
 	// aktualisiert einen Eintrag in der Tabelle Bildung
 	public boolean updateBildung(long id, ContentValues updates) {
 		return db.update(TABLE_BILDUNG, updates, BILDUNG_ID + "=" + id, null) > 0;
 	}
-	
-	
+
 	/**
 	 * @param bildung
 	 * @param result
@@ -122,7 +175,7 @@ public class BildungDB implements LebenslaufDB {
 		bildung.setDatumVon(result.getString(5));
 		bildung.setDatumBis(result.getString(6));
 		bildung.setPersID(result.getLong(7));
-		
-		return bildung;	
+
+		return bildung;
 	}
 }
