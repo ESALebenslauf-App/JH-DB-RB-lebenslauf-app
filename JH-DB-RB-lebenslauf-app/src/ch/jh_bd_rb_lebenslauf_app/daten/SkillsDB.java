e32fa359f7496f87e06adf81a8e2a2925e8c4be8
jherzig <info@jherzig.ch>
2014-06-12 19:34:35 +0200

    Datenbank Funktionalitäten für Skills erstellt und eingeführt inklusive Tests

--- JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/daten/SkillsDB.java
+++ JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/daten/SkillsDB.java
@@ -1,15 +1,17 @@
 package ch.jh_bd_rb_lebenslauf_app.daten;
 
+import java.util.ArrayList;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-public class SkillsDB implements LebenslaufDB{
-	
+public class SkillsDB implements LebenslaufDB {
+
 	private final DBHelper dbHelper;
 	private SQLiteDatabase db;
-	
+
 	public SkillsDB(Context context) {
 		dbHelper = new DBHelper(context);
 	}
@@ -24,43 +26,168 @@ public class SkillsDB implements LebenslaufDB{
 		dbHelper.close();
 	}
 
-	
-	// Skills ausgeben
-	public Cursor getSkills(long id) {
-		Cursor result = db.query(TABLE_SKILLS, LebenslaufDB.PROJECTION_SKILLS, SKILLS_ID + "=" + id, null, null, null, null);
-		boolean found = result.moveToFirst();
-		if (found) {
-			return result;
-		}
-		else {
+	/**
+	 * Läd anhand der ID im Objekt bildung die dazugehörigen Daten aus der DB
+	 * und füllt diese ins OBjekt ab.
+	 * 
+	 * @param skills
+	 * @return Skills mit Daten aus DB
+	 */
+	public SkillsData getBildung(SkillsData skills) {
+		String[] dbID = new String[1];
+		dbID[0] = skills.getID().toString();
+
+		Cursor result = db.query(false, TABLE_SKILLS, PROJECTION_SKILLS,
+				SKILLS_ID + "=?", dbID, null, null, null, null);
+
+		if (result.moveToFirst()) {
+			return setSkills(skills, result);
+		} else {
 			result.close();
 			return null;
 		}
 	}
-	
-	public Cursor getAllCursor() {
-		return db.query(TABLE_SKILLS, new String[] { SKILLS_ID, SKILLS_WAS, SKILLS_AUSMASS, SKILLS_ZERTIFIKAT}, null, null, null, null, null);
-	}
-	
-	// loescht einen Eintrag in der Tabelle Skills
-		public boolean deleteSkills(long id) {
-			return db.delete(TABLE_SKILLS, SKILLS_ID + "=" + id, null) > 0;
-	}
-		
-	
-	// erzeugt einen Eintrag in der Tabelle Skills
-	public long insertSkills(String was, String ausmass, String zertifikat) {
-		ContentValues values = new ContentValues();
-		values.put(SKILLS_WAS, was);
-		values.put(SKILLS_AUSMASS, ausmass);
-		values.put(SKILLS_ZERTIFIKAT, zertifikat);
-		
-		long id = db.insert(TABLE_SKILLS, null, values);
-		return id;
+
+	/**
+	 * Es werden alle Skils aus der DB geladen bei denen die where bedingen
+	 * erfühlt sind. Die Spallte der Bedingen muss mitangeben werden und der
+	 * wert der Bedingen im Skills Objekt.
+	 * 
+	 * @param skills
+	 * @param columWhere
+	 *            Colum Name der where abfrage
+	 * @return ArrayList<Skills> alle treffer
+	 */
+	/**
+	 * @param skills
+	 * @param columWhere
+	 * @return
+	 */
+	public ArrayList<SkillsData> getSkillsRows(SkillsData skills, String columWhere) {
+		String[] dbWhere = new String[1];
+		String dbColWhere = SKILLS_ID;
+
+		switch (columWhere) {
+		case SKILLS_ID:
+			dbWhere[0] = skills.getID().toString();
+			dbColWhere = SKILLS_ID;
+			break;
+		case SKILLS_WAS:
+			dbWhere[0] = skills.getWas().toString();
+			dbColWhere = SKILLS_WAS;
+			break;
+		case SKILLS_AUSMASS:
+			dbWhere[0] = skills.getAusmass().toString();
+			dbColWhere = SKILLS_AUSMASS;
+			break;
+		case SKILLS_ZERTIFIKAT:
+			dbWhere[0] = skills.getZertifikat().toString();
+			dbColWhere = SKILLS_ZERTIFIKAT;
+			break;
+
+		default:
+			break;
+		}
+
+		Cursor result = db.query(false, TABLE_SKILLS, PROJECTION_SKILLS,
+				dbColWhere + "=?", dbWhere, null, null, null, null);
+
+		ArrayList<SkillsData> skillsArr = new ArrayList<SkillsData>();
+		if (result.moveToFirst()) {
+			while (!result.isAfterLast()) {
+				SkillsData skill = new SkillsData(result.getLong(0));
+				skillsArr.add(setSkills(skill, result));
+				result.moveToNext();
+			}
+		}
+		return skillsArr;
 	}
 
-	// aktualisiert einen Eintrag in der Tabelle Skills
-	public boolean updateSkills(long id, ContentValues updates) {
-		return db.update(TABLE_SKILLS, updates, SKILLS_ID + "=" + id, null) > 0;
+	/**
+	 * Giebt alle Zeilen in der DB zurück.
+	 * 
+	 * @return ArrayList<Skills>
+	 */
+	public ArrayList<SkillsData> getAllSkills() {
+		ArrayList<SkillsData> skills = new ArrayList<SkillsData>();
+
+		Cursor result = db.query(false, TABLE_SKILLS, PROJECTION_SKILLS, null,
+				null, null, null, null, null);
+
+		if (result.moveToFirst()) {
+			while (!result.isAfterLast()) {
+				SkillsData skill = new SkillsData(result.getLong(0));
+				skills.add(setSkills(skill, result));
+				result.moveToNext();
+			}
+		}
+		return skills;
+	}
+
+	/**
+	 * loescht einen Eintrag in der Tabelle Skills anhand der ID
+	 * 
+	 * @param skills
+	 * @return boolean konnte der Eintrag geloescht werden
+	 */
+	public boolean deleteSkills(SkillsData skills) {
+		String[] dbID = new String[1];
+		dbID[0] = skills.getID().toString();
+		return db.delete(TABLE_SKILLS, SKILLS_ID + "=?", dbID) > 0;
+	}
+
+	/**
+	 * erzeugt einen Eintrag in der Tabelle Bildung
+	 * 
+	 * @param skills
+	 * @return Skills mit Daten aus DB abgefuehlt
+	 */
+	public SkillsData insertSkills(SkillsData skills) {
+		ContentValues values = new ContentValues();
+
+		values = putContentValues(skills, values);
+
+		skills.setID(db.insert(TABLE_SKILLS, null, values));
+		return skills;
+	}
+
+	/**
+	 * Das übergebene Bildungs Objekt wird anhand der ID Aktualisiert es werden
+	 * alle einträge des Objekts Aktualisiert.
+	 * 
+	 * @param bildung
+	 * @return
+	 */
+	public SkillsData updateSkills(SkillsData skills) {
+
+		ContentValues values = new ContentValues();
+		values = putContentValues(skills, values);
+
+		if (skills.getID() > 0) {
+			db.update(TABLE_SKILLS, values, "_id=?", new String[] { skills
+					.getID().toString() });
+
+			return getBildung(skills);
+		} else {
+			return skills;
+		}
+	}
+
+	private ContentValues putContentValues(SkillsData skills, ContentValues values) {
+		values.put(SKILLS_WAS, skills.getWas());
+		values.put(SKILLS_AUSMASS, skills.getAusmass());
+		values.put(SKILLS_ZERTIFIKAT, skills.getZertifikat());
+		values.put(SKILLS_PERS_ID, skills.getPers_id());
+
+		return values;
+	}
+
+	private SkillsData setSkills(SkillsData skills, Cursor result) {
+		skills.setWas(result.getString(1));
+		skills.setAusmass(result.getString(2));
+		skills.setZertifikat(result.getString(3));
+		skills.setPers_id(result.getLong(4));
+
+		return skills;
 	}
 }
---
+++ JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/daten/SkillsData.java
@@ -0,0 +1,72 @@
+package ch.jh_bd_rb_lebenslauf_app.daten;
+
+public class SkillsData implements Cloneable{
+
+	private Long ID;
+	private Long pers_id;
+	private String was;
+	private String ausmass;
+	private String zertifikat;
+
+	public SkillsData(String was, String ausmass, String zertifikat) {
+		super();
+		this.was = was;
+		this.ausmass = ausmass;
+		this.zertifikat = zertifikat;
+	}
+
+	public SkillsData(Long iD) {
+		super();
+		ID = iD;
+	}
+
+	public SkillsData clone() {
+		SkillsData theClone = null;
+		try {
+			theClone = (SkillsData) super.clone();
+		} catch (CloneNotSupportedException e) {
+		}
+		return theClone;
+	}
+	
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public Long getPers_id() {
+		return pers_id;
+	}
+
+	public void setPers_id(Long pers_id) {
+		this.pers_id = pers_id;
+	}
+
+	public String getWas() {
+		return was;
+	}
+
+	public void setWas(String was) {
+		this.was = was;
+	}
+
+	public String getAusmass() {
+		return ausmass;
+	}
+
+	public void setAusmass(String ausmass) {
+		this.ausmass = ausmass;
+	}
+
+	public String getZertifikat() {
+		return zertifikat;
+	}
+
+	public void setZertifikat(String zertifikat) {
+		this.zertifikat = zertifikat;
+	}
+
+}
