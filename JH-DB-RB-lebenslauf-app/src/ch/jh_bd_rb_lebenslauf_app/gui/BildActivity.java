c3dcaedde198b81d93e03b686397e2c57860c2ab
jherzig <info@jherzig.ch>
2014-06-22 09:22:25 +0200

    BildActivity Formatierung überarbeitet

--- JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/gui/BildActivity.java
+++ JH-DB-RB-lebenslauf-app/src/ch/jh_bd_rb_lebenslauf_app/gui/BildActivity.java
@@ -11,13 +11,13 @@ import ch.jh_bd_rb_lebenslauf_app.listener.BildListener;
 import ch.jh_bd_rb_lebenslauf_app.listener.HochladenListener;
 import ch.jh_bd_rb_lebenslauf_app.resource.FileConst;
 import ch.jh_bd_rb_lebenslauf_app.resource.StringConst;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
@@ -41,28 +41,17 @@ import android.widget.Toast;
 public class BildActivity extends FragmentActivity {
 
 	private Bitmap image = null;
-
 	private String imageString = "";
-
-	private Button btnBerufserfahrung;
 	private ImageButton btnCamera;
 	private ImageButton btnHochladen;
 	private Spinner spinnerAnrede;
 	private EditText txt_vorname;
-
 	private EditText txt_name;
 	private EditText txt_adresse;
 	private EditText text_edit_plz;
 	private EditText txt_Edit_ort;
 	private Button btnGeburtsdatum;
-	public Button getBtnGeburtsdatum() {
-		return btnGeburtsdatum;
-	}
-
-	public void setBtnGeburtsdatum(Button btnGeburtsdatum) {
-		this.btnGeburtsdatum = btnGeburtsdatum;
-	}
-
+	private Button btnBerufserfahrung;
 	private Long persID;
 	private boolean save = false;
 
@@ -71,44 +60,10 @@ public class BildActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bild);
 		this.persID = getIntent().getLongExtra(StringConst.getPesrid(), 0);
+
 		initActivityElemente();
 		initActivityListener();
 		loadData();
-	}
-
-	private void loadData() {
-		if (persID > 0) {
-			PersonalienData pers = new PersonalienData(persID);
-			PersonalienDB persDB = new PersonalienDB(this);
-			persDB.open();
-			pers = persDB.getPersonalien(pers);
-			getTxt_name().setText(pers.getName());
-			getTxt_vorname().setText(pers.getVorname());
-			getTxt_adresse().setText(pers.getStrasse());
-			getText_edit_plz().setText(pers.getPlz());
-			getTxt_Edit_ort().setText(pers.getOrt());
-			getBtnGeburtsdatum().setText(pers.getDate());
-
-
-			Resources res = getResources();
-			String[] anrede = res.getStringArray(R.array.anrede_array);
-
-			if (anrede[0].equals(pers.getAnrede())) {
-				getSpinnerAnrede().setSelection(0);
-			}
-			if (anrede[1].equals(pers.getAnrede())) {
-				getSpinnerAnrede().setSelection(1);
-			}
-			if (anrede[2].equals(pers.getAnrede())) {
-				getSpinnerAnrede().setSelection(2);
-			}
-
-		}
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
 	}
 
 	private void initActivityElemente() {
@@ -125,14 +80,6 @@ public class BildActivity extends FragmentActivity {
 		setSpinnerAnrede((Spinner) findViewById(R.id.spinnerAnrede));
 	}
 
-	public Button getBtnBerufserfahrung() {
-		return btnBerufserfahrung;
-	}
-
-	public void setBtnBerufserfahrung(Button btnBerufserfahrung) {
-		this.btnBerufserfahrung = btnBerufserfahrung;
-	}
-
 	private void initActivityListener() {
 		btnBerufserfahrung.setOnClickListener(new OnClickListener() {
 
@@ -143,17 +90,47 @@ public class BildActivity extends FragmentActivity {
 		});
 
 		btnGeburtsdatum.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				DialogFragment newFragment = new DatePickerFragment();
-				newFragment.show(getSupportFragmentManager(), StringConst.DATEPICKERGEBURTSDATUM);
+				newFragment.show(getSupportFragmentManager(),
+						StringConst.DATEPICKERGEBURTSDATUM);
 			}
 		});
-		
+
 		btnCamera.setOnClickListener(new BildListener(this));
 		btnHochladen.setOnClickListener(new HochladenListener(this));
 
+	}
+
+	private void loadData() {
+		if (persID > 0) {
+			PersonalienData pers = new PersonalienData(persID);
+			PersonalienDB persDB = new PersonalienDB(this);
+			persDB.open();
+			pers = persDB.getPersonalien(pers);
+
+			getTxt_name().setText(pers.getName());
+			getTxt_vorname().setText(pers.getVorname());
+			getTxt_adresse().setText(pers.getStrasse());
+			getText_edit_plz().setText(pers.getPlz());
+			getTxt_Edit_ort().setText(pers.getOrt());
+			getBtnGeburtsdatum().setText(pers.getDate());
+
+			Resources res = getResources();
+			String[] anrede = res.getStringArray(R.array.anrede_array);
+
+			if (anrede[0].equals(pers.getAnrede())) {
+				getSpinnerAnrede().setSelection(0);
+			}
+			if (anrede[1].equals(pers.getAnrede())) {
+				getSpinnerAnrede().setSelection(1);
+			}
+			if (anrede[2].equals(pers.getAnrede())) {
+				getSpinnerAnrede().setSelection(2);
+			}
+		}
 	}
 
 	/**
@@ -191,17 +168,6 @@ public class BildActivity extends FragmentActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	/*
-	 * @Override protected void onDestroy() { super.onDestroy(); if (!save) {
-	 * datenSpeichern(); } }
-	 * 
-	 * @Override protected void onStop() { super.onStop(); if (!save) {
-	 * datenSpeichern(); } }
-	 */
-
-	/**
-	 * Daten können Persistent gespeichert werden
-	 */
 	private Long datenSpeichern() {
 		PersonalienData pers;
 
@@ -272,6 +238,87 @@ public class BildActivity extends FragmentActivity {
 		return pers;
 	}
 
+	/**
+	 * 
+	 * @author rbuess
+	 * 
+	 */
+	// Diese Methode handelt das geschossene(oder aus der Galry gewählte) Foto
+	// vom BildListener
+	// Der BildListener sendet das Ergebnis zurück an die BildActivity.
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		// Wenn ein Foto neu geschossen wurde
+		if (requestCode == 1 && resultCode == RESULT_OK) {
+			image = (Bitmap) data.getExtras().get("data");
+			Toast toast = Toast.makeText(this, "Foto gespeichert",
+					Toast.LENGTH_LONG);
+			toast.show();
+		}
+		// Wenn ein Foto aus der Galery ausgewählt wurde
+		if (requestCode == 2 && resultCode == RESULT_OK) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String filePath = cursor.getString(columnIndex);
+			cursor.close();
+
+			image = BitmapFactory.decodeFile(filePath);
+
+			Toast toast = Toast.makeText(this, "Foto gespeichert",
+					Toast.LENGTH_LONG);
+			toast.show();
+		}
+	}
+
+	/**
+	 * 
+	 * @author rbuess
+	 * 
+	 */
+	// Diese Methode Speichert das Foto und gibt die Personen ID zu der
+	// Bezeichnung hinzu
+	public String saveImage(Long id) {
+
+		String ID = id.toString();
+
+		String fotoName = "Foto.jpg";
+
+		String pfad = FileConst.getPdfPath();
+
+		String fileName = pfad + "/" + ID + fotoName;
+
+		File fotoFile = new File(fileName);
+
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+		byte[] byteArray = stream.toByteArray();
+		try {
+			FileOutputStream fos = new FileOutputStream(fotoFile);
+			fos.write(byteArray);
+			fos.close();
+		} catch (Exception error) {
+		}
+
+		imageString = fotoName;
+		return imageString;
+
+	}
+
+	public Button getBtnBerufserfahrung() {
+		return btnBerufserfahrung;
+	}
+
+	public void setBtnBerufserfahrung(Button btnBerufserfahrung) {
+		this.btnBerufserfahrung = btnBerufserfahrung;
+	}
+
 	public EditText getTxt_vorname() {
 		return txt_vorname;
 	}
@@ -312,7 +359,6 @@ public class BildActivity extends FragmentActivity {
 		this.txt_Edit_ort = txt_Edit_ort;
 	}
 
-
 	public Spinner getSpinnerAnrede() {
 		return spinnerAnrede;
 	}
@@ -321,67 +367,11 @@ public class BildActivity extends FragmentActivity {
 		this.spinnerAnrede = spinnerAnrede;
 	}
 
-	// Diese Methode handelt das geschossene(oder aus der Galry gewählte) Foto
-	// vom BildListener
-	// Der BildListener sendet das Ergebnis zurück an die BildActivity.
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-		// Wenn ein Foto neu geschossen wurde
-		if (requestCode == 1 && resultCode == RESULT_OK) {
-			image = (Bitmap) data.getExtras().get("data");
-			Toast toast = Toast.makeText(this, "Foto gespeichert",
-					Toast.LENGTH_LONG);
-			toast.show();
-		}
-		// Wenn ein Foto aus der Galery ausgewählt wurde
-		if (requestCode == 2 && resultCode == RESULT_OK) {
-			Uri selectedImage = data.getData();
-			String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-			Cursor cursor = getContentResolver().query(selectedImage,
-					filePathColumn, null, null, null);
-			cursor.moveToFirst();
-
-			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-			String filePath = cursor.getString(columnIndex);
-			cursor.close();
-
-			image = BitmapFactory.decodeFile(filePath);
-
-			Toast toast = Toast.makeText(this, "Foto gespeichert",
-					Toast.LENGTH_LONG);
-			toast.show();
-		}
+	public Button getBtnGeburtsdatum() {
+		return btnGeburtsdatum;
 	}
 
-	// Diese Methode Speichert das Foto und gibt die Personen ID zu der
-	// Bezeichnung hinzu
-	public String saveImage(Long id) {
-
-		String ID = id.toString();
-
-		String fotoName = "Foto.jpg";
-
-		String pfad = FileConst.getPdfPath();
-
-		String fileName = pfad + "/" + ID + fotoName;
-
-		File fotoFile = new File(fileName);
-
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-		byte[] byteArray = stream.toByteArray();
-		try {
-			FileOutputStream fos = new FileOutputStream(fotoFile);
-			fos.write(byteArray);
-			fos.close();
-		} catch (Exception error) {
-		}
-
-		imageString = fotoName;
-		return imageString;
-
+	public void setBtnGeburtsdatum(Button btnGeburtsdatum) {
+		this.btnGeburtsdatum = btnGeburtsdatum;
 	}
-
 }
